9be991f11125e2978fbfedc9c278d8502291d88c
dbury2 <dbury200@gmail.com>
2015-10-17 12:01:35 +0000

    po zrobieniu git ignore

--- index.php
+++ index.php
@@ -2,7 +2,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors',1);
 
-require_once 'vendor/Employee.php';
+//require_once 'vendor/Employee.php';
+
+spl_autoload_register('Autoloader');
+function Autoloader($className)
+{
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $className);
+    require_once 'vendor/'.$class.'.php';
+}
+
+use Academy\Employee;
+
 $john = new Employee("john", "smith", 22, "f");
 $jane = new Employee("jane", "smith", 23, "m");
 echo $john->hello();
